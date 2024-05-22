982b73a0abb54d285492ec7f954f71295c46073f
aluisio <aaluisio309@gmail.com>
2024-05-22 11:56:22 -0300

    tentativa sem sucesso de js

--- js/main.js
+++ js/main.js
@@ -1,28 +1,8 @@
+
 //home do cabecario
-const home =document.getElementById('home');
+const home = document.getElementById('home');
 //cardapio do cabecario
 const cardapio = document.getElementById('cardapio');
 //container home
 const home_page = document.querySelector(".container_home");
 
-
-
-function homeAtiva(){
-    let homeAtv = home.getAttribute("ativo") === "true";
-    
-    if (homeAtv){
-        home.style.textDecoration = "underline";
-        home_page.style.display = "flex"
-    }else{
-        cardapio.style.textDecoration = "underline";
-      
-    }
-}
-
-setInterval(1000, homeAtiva())
-
-
-function cardapioAtivo(){
-   cardapio.ativo = "true";
-   home_page.style.display = "none";
-}
