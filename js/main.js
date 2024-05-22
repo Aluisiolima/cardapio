6f1653e8314ab5d251606aa80ca8964cb7d4c9ff
aluisio <aaluisio309@gmail.com>
2024-05-22 15:08:22 -0300

    caberario js feito agora pode muda para tela de cadapio sem sair ou atualiza

--- js/main.js
+++ js/main.js
@@ -5,4 +5,18 @@ const home = document.getElementById('home');
 const cardapio = document.getElementById('cardapio');
 //container home
 const home_page = document.querySelector(".container_home");
+//container cardapio
+const cardapio_page = document.querySelector(".container_cardapio");
 
+function homeAtive(){
+    home_page.style.display = "flex";
+    home.style.textDecoration = "underline";
+    cardapio_page.style.display = "none";
+    cardapio.style.textDecoration = "none";
+}
+function cardapioAtive(){
+    home_page.style.display = "none";
+    home.style.textDecoration = "none";
+    cardapio.style.textDecoration = "underline";
+    cardapio_page.style.display = "flex";
+}
