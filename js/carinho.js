3b367b5fd7a186075edc01a033fee7476e89fadc
aluisio <aaluisio309@gmail.com>
2024-07-02 20:03:16 -0300

    ajuste de logica das telass de pagamentos front

--- js/carinho.js
+++ js/carinho.js
@@ -1,7 +1,22 @@
 var local = true;
+const tela = document.querySelector(".container_pagamentos");
+const telaCarinho = document.querySelector(".container_carinho");
+var mesa = document.getElementById("mesa");
+var entrega = document.getElementById("entrega");
 
 function mudarValorDaVariavel() {
-    local!=local;
+    entrega.addEventListener("click", ()=>{
+      entrega.style.background = "green";
+      mesa.style.background = "antiquewhite";
+      local = true;
+      return local;
+    });
+    mesa.addEventListener("click", ()=>{
+      mesa.style.background = "green";
+      entrega.style.background = "antiquewhite";
+      local = false;
+      return local;
+    })
     return local;
 }
 
@@ -9,13 +24,17 @@ function telaDeEmtrega(){
     let typeDelivery = local
 
     switch(typeDelivery){
-        case true:  tela1();
-        case false: tela2();
+        case true: 
+          tela1(); 
+          break;
+        case false:
+          tela2();
+          break;
     }
 }
 
 function tela1(){
-    const tela = document.querySelector(".container_pagamentos");
+    telaCarinho.style.display = 'none';
 
     tela.innerHTML= `
     <div class="dados">
@@ -23,6 +42,35 @@ function tela1(){
       <input type="text" placeholder="bairro">
       <input type="text" placeholder="rua">
       <input type="number" placeholder="numero da casa">
+    </div>
+    <div class="produtos_escolhidos">
+
+    </div>
+    <h4>formas de pagamento</h4>
+    <div class="formas_De_Pagamento">
+      <div class="meios_de_pagamento">
+        <img src="./img/pix.svg" alt="pix" height="50">
+      </div>
+      <div class="meios_de_pagamento">
+        <img src="./img/cartao.svg" alt="pix" height="50">
+      </div>
+      <div class="meios_de_pagamento">
+        <img src="./img/dinheiro.svg" alt="pix" height="50">
+      </div>
+    </div>
+    <div class="finaliza_pedido">
+      <p>valor =</p>
+      <button>compra</button>
+    </div>
+    `
+}
+function tela2(){
+  telaCarinho.style.display = 'none';
+  tela.innerHTML= `
+    <div class="dados">
+      <input type="text" placeholder="nome">
+      <input type="text" placeholder="mesa">
+      <input type="number" placeholder="numero da mesa">
     </div>
     <div class="produtos_escolhidos">
 
