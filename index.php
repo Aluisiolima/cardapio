c4256861e0a90e0b5b4bfea24662e425de1095f3
aluisio <aaluisio309@gmail.com>
2024-07-18 17:03:38 -0300

    ola o dinamismo vindpo mlk ajax la vamos nois

--- index.php
+++ index.php
@@ -17,11 +17,25 @@
 <div class="container">
     <!--cabecario ,logo e links de acesso rapiodo-->
    <?php include "./components/nav.php"; ?>
-
+   <div id="content-area"></div>
    <?php include "./components/footer.php"; ?>
 </div>
 
-
 </body>
-<script src="./js/cabercario.js" defer></script>
+<script>
+        function loadContent(page) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', page, true);
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    document.getElementById('content-area').innerHTML = xhr.responseText;
+                } else if (xhr.readyState == 4) {
+                    document.getElementById('content-area').innerHTML = '<p>Erro ao carregar o conteúdo.</p>';
+                }
+            };
+            xhr.send();
+        }
+        loadContent('home.html');
+    </script>
+<script src="./js/cardapio.js"></script>
 </html>
