c7c789e2457a969da32cbccdac152e17b3e76ba8
Aluisio Lima <aaluisio309@gmail.com>
2025-04-25 13:46:07 -0300

    feat(test): desenvolvendo os teste unitarios da aplicacao

--- src/app/components/CardEntrega/Delivery.tsx
+++ src/app/components/CardEntrega/Delivery.tsx
@@ -24,7 +24,8 @@ export const Delivery: React.FC<{ data: Empresa | null; onTroca: (name: string) 
   data,
   onTroca,
 }) => {
-  const [formData, setFormData] = useState<Entrega>({nome: '',
+  const [formData, setFormData] = useState<Entrega>({
+    nome: '',
     bairro: '',
     rua: '',
     numero_casa: '',
